52e46586ea3a2f65002082764521f2486ac46442
Felix D <felix.divo@gmail.com>
2019-05-13 00:40:29 +0200

    implement possibility to apply changes immediately with commit()

--- kotlinpreferences-lib/src/main/java/com/marcinmoskala/kotlinpreferences/KotlinPreferences.kt
+++ kotlinpreferences-lib/src/main/java/com/marcinmoskala/kotlinpreferences/KotlinPreferences.kt
@@ -8,26 +8,26 @@ import com.marcinmoskala.kotlinpreferences.bindings.PropertyWithBackupNullable
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KClass
 
-inline fun <reified T : Any> bindToPreferenceField(default: T?, key: String? = null): ReadWriteProperty<SharedPreferences, T>
-        = bindToPreferenceField(T::class, default, key)
+inline fun <reified T : Any> bindToPreferenceField(default: T?, key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T>
+        = bindToPreferenceField(T::class, default, key, applyAsync)
 
-inline fun <reified T : Any> bindToPreferenceFieldNullable(key: String? = null): ReadWriteProperty<SharedPreferences, T?>
-        = bindToPreferenceFieldNullable(T::class, key)
+inline fun <reified T : Any> bindToPreferenceFieldNullable(key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T?>
+        = bindToPreferenceFieldNullable(T::class, key, applyAsync)
 
-inline fun <reified T : Any> bindAsPropertyWithBackup(default: T?, key: String? = null): ReadWriteProperty<SharedPreferences, T>
-        = bindAsPropertyWithBackup(T::class, default, key)
+inline fun <reified T : Any> bindAsPropertyWithBackup(default: T?, key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T>
+        = bindAsPropertyWithBackup(T::class, default, key, applyAsync)
 
-inline fun <reified T : Any> bindAsPropertyWithBackupNullable(key: String? = null): ReadWriteProperty<SharedPreferences, T?>
-        = bindAsPropertyWithBackupNullable(T::class, key)
+inline fun <reified T : Any> bindAsPropertyWithBackupNullable(key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T?>
+        = bindAsPropertyWithBackupNullable(T::class, key, applyAsync)
 
-fun <T : Any> bindToPreferenceField(clazz: KClass<T>, default: T?, key: String? = null): ReadWriteProperty<SharedPreferences, T>
-        = PreferenceFieldDelegate(clazz, default, key)
+fun <T : Any> bindToPreferenceField(clazz: KClass<T>, default: T?, key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T>
+        = PreferenceFieldDelegate(clazz, default, key, applyAsync)
 
-fun <T : Any> bindToPreferenceFieldNullable(clazz: KClass<T>, key: String? = null): ReadWriteProperty<SharedPreferences, T?>
-        = PreferenceFieldDelegateNullable(clazz, key)
+fun <T : Any> bindToPreferenceFieldNullable(clazz: KClass<T>, key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T?>
+        = PreferenceFieldDelegateNullable(clazz, key, applyAsync)
 
-fun <T : Any> bindAsPropertyWithBackup(clazz: KClass<T>, default: T?, key: String? = null): ReadWriteProperty<SharedPreferences, T>
-        = PropertyWithBackup(clazz, default, key)
+fun <T : Any> bindAsPropertyWithBackup(clazz: KClass<T>, default: T?, key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T>
+        = PropertyWithBackup(clazz, default, key, applyAsync)
 
-fun <T : Any> bindAsPropertyWithBackupNullable(clazz: KClass<T>, key: String? = null): ReadWriteProperty<SharedPreferences, T?>
-        = PropertyWithBackupNullable(clazz, key)
+fun <T : Any> bindAsPropertyWithBackupNullable(clazz: KClass<T>, key: String? = null, applyAsync: Boolean = true): ReadWriteProperty<SharedPreferences, T?>
+        = PropertyWithBackupNullable(clazz, key, applyAsync)
